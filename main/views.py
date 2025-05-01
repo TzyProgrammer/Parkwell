43c892c5dbc72d02e448d367fd01c6ec5754bbb1
Shevirond <ardian.zahran@gmail.com>
2025-05-01 21:01:16 +0700

    menambahkan backend login dan register dan modify login html dan register html

--- main/views.py
+++ main/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import CustomUser
+from django.contrib import messages
+import logging
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
+def register_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        phone_number = request.POST['phone_number']
+        
+        if CustomUser.objects.filter(username=username).exists():
+            messages.error(request, "Username already taken")
+            return redirect('register')
+        
+        user = CustomUser.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            phone_number=phone_number
+        )
+        login(request, user)
+        return redirect('login')
+    return render(request, 'register.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            logger.debug(f"User {user.username} logged in successfully")
+            return redirect('home')
+        else:
+            logger.warning("Authentication failed for email: %s", username)
+            messages.error(request, 'Invalid email or password')
+            return redirect('login')
+    return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def home_view(request):
+    return render(request, 'home.html')
