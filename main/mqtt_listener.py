9a70fcae40d7140045ca2a302c94f389ae83e7b9
TeamPBLIF18OnLinux <pblif18@gmail.com>
2025-05-27 01:21:59 +0700

    Add 4 Sensors : Compleated

--- main/mqtt_listener.py
+++ main/mqtt_listener.py
@@ -1,33 +1,41 @@
 import os
 import django
 import paho.mqtt.client as mqtt
-import json
 
-# Setup Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Parkwell.settings")
 django.setup()
 
 from main.models import Spot
 
+# Map topik ke spot_number
+topic_to_spot = {
+    "parkir/slot1": "1",
+    "parkir/slot2": "2",
+    "parkir/slot3": "3",
+    "parkir/slot4": "4",
+}
+
 def on_connect(client, userdata, flags, rc):
-    print("Terhubung ke broker MQTT")
-    client.subscribe("parkir/slot1")
+    print("Terhubung ke MQTT")
+    for topic in topic_to_spot.keys():
+        client.subscribe(topic)
 
 def on_message(client, userdata, msg):
     try:
+        topic = msg.topic
         payload = msg.payload.decode()
         distance = float(payload)
-        print(f"Jarak diterima: {distance} cm")
+        spot_number = topic_to_spot[topic]
+        print(f"Data dari {topic} = {distance} cm")
 
-        spot = Spot.objects.get(spot_number="1")
+        spot = Spot.objects.get(spot_number=spot_number)
         spot.update_status_from_distance(distance)
-        print(f"Status spot 1 diperbarui menjadi {spot.status}")
+        print(f"Slot {spot_number} diupdate ke {spot.status}")
+        print("="*30)
 
-    except Spot.DoesNotExist:
-        print("Spot dengan nomor 1 tidak ditemukan.")
     except Exception as e:
-        print("Gagal memproses pesan:", e)
-
+        print("Gagal memproses:", e)
+    
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
