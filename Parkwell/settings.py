943254e642144a5346f26454616bc56c544531a9
TzyProgrammer <classroomtarich@gmail.com>
2025-07-14 08:35:36 +0700

    git commit -m

--- Parkwell/settings.py
+++ Parkwell/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-e_cigyy#ip2fn%03g4$=jw1)a_50dop2i0-_x@79jby&o39*00
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
